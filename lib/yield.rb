306145a2768a4b6a5cccbcceed443d190a6f9110
Casey Scarborough <caseyscarborough@gmail.com>
2013-08-06 23:31:13 -0400

    Add basic gem functionality. Add -f option for passing in a filename with parameter. Renders README.md by default at localhost:4567.

--- lib/yield.rb
+++ lib/yield.rb
@@ -1,5 +1,23 @@
-require "yield/version"
+require "sinatra"
+require "octokit"
+require_relative "yield/version"
 
 module Yield
-  # Your code goes here...
+
+  class Markdown < Sinatra::Base
+
+    @@content = nil
+    @@filename = nil
+
+    def self.render(filename = 'README.md')
+      @@filename = filename
+      @@content = Octokit.markdown(File.read(@@filename), mode: 'gfm')
+    end
+
+    get '/' do
+      erb :index, locals: { content: @@content, filename: @@filename }  
+    end
+  
+  end
+
 end
