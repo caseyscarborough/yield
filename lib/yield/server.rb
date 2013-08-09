c28fc10956fbcd550c2852869c4fadc25d71b7a9
Casey Scarborough <caseyscarborough@gmail.com>
2013-08-09 16:38:45 -0400

    Remove unnecessary code from lib/yield/server.rb.

--- lib/yield/server.rb
+++ lib/yield/server.rb
@@ -1,45 +1,37 @@
 module Yield
   class Server < Sinatra::Base
 
-    set :server, %w[webrick]
+    set :server, 'webrick'
     set :port, 4000
 
     # Override run! method from Sinatra
-    def self.run!(options = {})
-      set options
-      handler         = detect_rack_handler
-      handler_name    = handler.name.gsub(/.*::/, '')
-      server_settings = {}
-      handler.run self, server_settings.merge(Port: port, Host: bind, Logger: WEBrick::Log.new("/dev/null"), AccessLog: []) do |server|
-        unless handler_name =~ /cgi/i
-          $stderr.puts "=* Yield is serving your markdown at http://localhost:#{port}/"
-        end
-        [:INT, :TERM].each { |sig| trap(sig) { quit!(server, handler_name) } }
+    def self.run!()
+      settings = {}
+      detect_rack_handler.run self, settings.merge(Port: port, Host: bind, Logger: WEBrick::Log.new('/dev/null'), AccessLog: []) do |server|
+        $stderr.puts "=* Yield is serving your markdown at http://localhost:#{port}/"
+        [:INT, :TERM].each { |sig| trap(sig) { quit!(server) } }
         server.threaded = settings.threaded if server.respond_to? :threaded=
         set :running, true
         open_in_browser
         yield server if block_given?
       end
     rescue Errno::EADDRINUSE
-      $stderr.puts "=* Port #{port} is already being used!"
+      $stderr.puts "=* Port #{port} is already in use!"
     end
 
     # Override quit! method from Sinatra
-    def self.quit!(server, handler_name)
-      # Use Thin's hard #stop! if available, otherwise just #stop.
-      server.respond_to?(:stop!) ? server.stop! : server.stop
-      $stderr.puts "=* Stopping yield..." unless handler_name =~/cgi/i
+    def self.quit!(server)
+      server.stop
+      $stderr.puts '=* Stopping yield...'
     end
 
     get '/' do
-      @@content = Yield::Markdown.convert_to_html(File.read(Yield::Markdown.filename).to_s)
-      erb :index, locals: { content: @@content, filename: Yield::Markdown.filename }
+      filename = Markdown.filename
+      content = Markdown.convert_to_html(File.read(filename))
+      erb :index, locals: { content: content, filename: filename }
     end
 
-    not_found do
-      erb :'404', locals: { filename: params[:filename] }
-    end
-
+    not_found { erb :'404' }
 
     def self.open_in_browser
       Launchy.open("http://localhost:#{port}/")
