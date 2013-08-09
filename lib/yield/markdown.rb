b485989aef990fb5400cd95fb2971dd85e37c308
Casey Scarborough <caseyscarborough@gmail.com>
2013-08-09 16:56:16 -0400

    Refactor and remove unnecessary code from lib/yield/markdown.rb.

--- lib/yield/markdown.rb
+++ lib/yield/markdown.rb
@@ -1,33 +1,29 @@
 module Yield
   class Markdown < Sinatra::Base
 
-    @@content = nil
-    @@filename = nil
+    @content = nil
+    @filename = nil
 
     def self.init(filename = 'README.md')
-      @@filename = filename.to_s
-      original_stdout = $stdout
-      $stdout = fake = StringIO.new
-      unless File.exist?(@@filename)
-        abort "Filename '#{@@filename}' doesn't exist."
-      end
+      @filename = filename.to_s
+      $stdout = StringIO.new
+      abort "Filename '#{@filename}' doesn't exist." unless File.exist?(@filename)
     end
 
     def self.convert_to_html(text)
       uri = URI('https://api.github.com/markdown')
       req = Net::HTTP::Post.new(uri.to_s)
-      req.body = { :text => "#{text}", :mode => "gfm" }.to_json
-      
-      response = Net::HTTP.start(uri.host, uri.port, :use_ssl => true) { |http| http.request(req) }
+      req.body = { text: "#{text}", mode: 'gfm' }.to_json
+      response = Net::HTTP.start(uri.host, uri.port, use_ssl: true) { |http| http.request(req) }
       response.body
     end
 
-    def self.filename
-      @@filename
+    def self.filename 
+      @filename
     end
 
     def self.content
-      @@content
+      @content
     end
 
   end
