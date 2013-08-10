78e5faf9358ebd7d8b622073494044353b28562f
Casey Scarborough <caseyscarborough@gmail.com>
2013-08-10 09:15:33 -0400

    Minor refactoring to bin/yield and lib/yield.rb.

--- lib/yield.rb
+++ lib/yield.rb
@@ -1,12 +1,12 @@
-require "sinatra"
-require "launchy"
-require "net/https"
-require "uri"
-require "json"
+require 'sinatra'
+require 'launchy'
+require 'net/https'
+require 'uri'
+require 'json'
 
-require_relative "yield/version"
-require_relative "yield/markdown"
-require_relative "yield/server"
+require_relative 'yield/version'
+require_relative 'yield/markdown'
+require_relative 'yield/server'
 
 module Yield
 end
